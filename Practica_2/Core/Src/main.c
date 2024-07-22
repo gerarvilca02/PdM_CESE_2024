fc401622c73e891e3a0377cced806866a441b0f5
Gerardo Vilcamiza <gerardo.vilcamiza@ieee.org>
2024-07-22 05:24:46 -0600

    Se incluyó el Punto 3 que era opcional

---
+++ Practica_2/Core/Src/main.c
@@ -0,0 +1,168 @@
+#include "main.h"
+
+UART_HandleTypeDef huart2;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+
+void delayInit(delay_t * delay, tick_t duration)
+{
+    delay->duration = duration;
+    delay->running = false;
+}
+
+bool_t delayRead(delay_t * delay)
+{
+    if (!delay->running)
+    {
+        delay->startTime = HAL_GetTick();
+        delay->running = true;
+    }
+    else
+    {
+        if ((HAL_GetTick() - delay->startTime) >= delay->duration)
+        {
+            delay->running = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+void delayWrite(delay_t * delay, tick_t duration)
+{
+    delay->duration = duration;
+}
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+
+    delay_t ledDelay;
+    int patterns[][2] = {
+        {1000, 5},  // 1 segundo de periodo, 5 ciclos
+        {200, 5},   // 200 ms de periodo, 5 ciclos
+        {100, 5}    // 100 ms de periodo, 5 ciclos
+    };
+    int patternCount = sizeof(patterns) / sizeof(patterns[0]);
+    int currentPattern = 0;
+    int currentCycle = 0;
+
+    // Iniciar el retardo con la mitad del periodo para un duty cycle del 50%
+    delayInit(&ledDelay, patterns[currentPattern][0] / 2);
+
+    while (1)
+    {
+        if (delayRead(&ledDelay))
+        {
+            // Alternar el estado del LED
+            HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+
+            // Contar los ciclos de encendido/apagado
+            if (++currentCycle >= 2 * patterns[currentPattern][1])
+            {
+                currentCycle = 0;
+                // Cambiar al siguiente patrón después de completar el número de ciclos
+                if (++currentPattern >= patternCount)
+                {
+                    currentPattern = 0;
+                }
+            }
+
+            // Ajustar el retardo a la mitad del nuevo periodo para mantener el duty cycle del 50%
+            delayWrite(&ledDelay, patterns[currentPattern][0] / 2);
+        }
+    }
+}
+
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+static void MX_USART2_UART_Init(void)
+{
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+static void MX_GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+    GPIO_InitStruct.Pin = B1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LD2_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+}
+
+void Error_Handler(void)
+{
+    __disable_irq();
+    while (1)
+    {
+    }
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+}
+#endif
